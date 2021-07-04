3ecc2811ccfba1dd9f18a55417fe82d1ff23e692
WilliamChavula <rumbani@gmail.com>
2021-07-03 23:09:12 -0500

    refactored restaurant_detail.dart.

--- lib/screens/restaurant_detail.dart
+++ lib/screens/restaurant_detail.dart
@@ -38,97 +38,18 @@ class _RestaurantDetailState extends State<RestaurantDetail> {
             child: DelayedDisplay(
               delay: Duration(milliseconds: 500),
               slidingBeginOffset: const Offset(0.0, 0.0),
-              child: Container(
-                padding:
-                    const EdgeInsets.only(top: 8.0, left: 10.0, right: 8.0),
+              child: Padding(
+                padding: EdgeInsets.only(top: 8.0, left: 10.0, right: 8.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(widget.restaurantData.name,
                         style: kRestaurantDetailPageHeaderStyle.copyWith(
                             fontSize: 28)),
-                    Row(
-                      children: [
-                        Text('Location:',
-                            style: kListTileTextStyle.copyWith(
-                                fontWeight: FontWeight.bold)),
-                        const SizedBox(width: 5.0),
-                        _getListDataAndReturnWidget(widget.restaurantData.city)
-                      ],
-                    ),
-                    const SizedBox(height: 8.0),
-                    Text('Address:',
-                        style: kListTileTextStyle.copyWith(
-                            fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 4.0),
-                    Text(widget.restaurantData.address?.blantyreAddress,
-                        style: kListTileTextStyle),
-                    Text(widget.restaurantData.address?.lilongweAddress,
-                        style: kListTileTextStyle),
-                    const SizedBox(height: 8.0),
-                    Text('Phone:',
-                        style: kListTileTextStyle.copyWith(
-                            fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 4.0),
-                    Text(widget.restaurantData.phoneNumber?.blantyrePhoneNumber,
-                        style: kListTileTextStyle),
-                    Text(widget.restaurantData.phoneNumber?.lilongwePhoneNumber,
-                        style: kListTileTextStyle),
-                    const SizedBox(height: 8.0),
+                    _locationAndContactInfo(),
+                    const SizedBox(height: 16.0),
                     Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Overview',
-                            style: Theme.of(context)
-                                .textTheme
-                                .subtitle2
-                                .copyWith(
-                                    color: Color(0XFF424953), fontSize: 18.0),
-                          ),
-                          Text(
-                            widget.restaurantData.overview,
-                            style: kListTileTextStyle.copyWith(
-                              fontSize: 16.0,
-                              height: 1.8,
-                            ),
-                          ),
-                          Expanded(
-                            child: GridView.builder(
-                              itemCount:
-                                  widget.restaurantData.images?.length ?? 0,
-                              itemBuilder: (context, index) {
-                                return Container(
-                                  width: 40,
-                                  height: 40,
-                                  clipBehavior: Clip.antiAlias,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(4.0))),
-                                  child: Container(
-                                    width: 40,
-                                    height: 40,
-                                    child: CachedNetworkImage(
-                                      fit: BoxFit.cover,
-                                      imageUrl: imgs[index].stripWhiteSpace(),
-                                      placeholder: (context, _) =>
-                                          LoadingIndicatorWidget(
-                                        size: MediaQuery.of(context).size,
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              },
-                              gridDelegate:
-                                  SliverGridDelegateWithFixedCrossAxisCount(
-                                      crossAxisCount: 3,
-                                      crossAxisSpacing: 8,
-                                      mainAxisSpacing: 8),
-                            ),
-                          ),
-                        ],
-                      ),
+                      child: _overviewAndPhotos(context, imgs),
                     ),
                   ],
                 ),
@@ -140,33 +61,121 @@ class _RestaurantDetailState extends State<RestaurantDetail> {
     );
   }
 
+  Column _overviewAndPhotos(BuildContext context, List<String> imgs) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Info',
+            style: Theme.of(context)
+                .textTheme
+                .subtitle2
+                .copyWith(color: Color(0XFF424953), fontSize: 18.0),
+          ),
+          Text(
+            widget.restaurantData.overview,
+            style: kListTileTextStyle.copyWith(
+              fontSize: 16.0,
+              height: 1.8,
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              itemCount: widget.restaurantData.images?.length ?? 0,
+              itemBuilder: (context, index) => _buildGridImage(imgs, index),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3, crossAxisSpacing: 8, mainAxisSpacing: 8),
+            ),
+          ),
+        ],
+      );
+
+  Column _locationAndContactInfo() => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text('Location:',
+                  style:
+                      kListTileTextStyle.copyWith(fontWeight: FontWeight.bold)),
+              const SizedBox(width: 5.0),
+              _getListDataAndReturnWidget(widget.restaurantData.city)
+            ],
+          ),
+          const SizedBox(height: 12.0),
+          Text('Address:',
+              style: kListTileTextStyle.copyWith(fontWeight: FontWeight.bold)),
+          const SizedBox(height: 4.0),
+          _buildCityOrPhoneWidget(
+            textOne: widget.restaurantData.address?.blantyreAddress,
+            textTwo: widget.restaurantData.address?.lilongweAddress,
+          ),
+          const SizedBox(height: 12.0),
+          Text('Phone:',
+              style: kListTileTextStyle.copyWith(fontWeight: FontWeight.bold)),
+          const SizedBox(height: 4.0),
+          _buildCityOrPhoneWidget(
+            textOne: widget.restaurantData.phoneNumber?.blantyrePhoneNumber,
+            textTwo: widget.restaurantData.phoneNumber?.lilongwePhoneNumber,
+          ),
+        ],
+      );
+
+  Container _buildGridImage(List<String> imgs, int index) => Container(
+        width: 40,
+        height: 40,
+        clipBehavior: Clip.antiAlias,
+        decoration:
+            BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(4.0))),
+        child: Container(
+          width: 40,
+          height: 40,
+          child: CachedNetworkImage(
+            fit: BoxFit.cover,
+            imageUrl: imgs[index].stripWhiteSpace(),
+            placeholder: (context, _) => LoadingIndicatorWidget(
+              size: MediaQuery.of(context).size,
+            ),
+          ),
+        ),
+      );
+
   Widget _getListDataAndReturnWidget(List<dynamic> city) {
     Widget cityWidget;
 
     if (city.length > 1) {
-      // String firstCity = ;
-      // String secondCity = ;
-      cityWidget = _buildTextWidget(firstCity: city[0], secondCity: city[1]);
+      cityWidget = _buildTextWidget(textOne: city[0], textTwo: city[1]);
     } else {
-      // String firstCity = ;
-      cityWidget = _buildTextWidget(firstCity: city[0]);
+      cityWidget = _buildTextWidget(textOne: city[0]);
     }
-
     return cityWidget;
   }
 
-  Text _buildTextWidget({@required String firstCity, String secondCity = ''}) {
-    var cityString;
-    if (secondCity.isNotEmpty) {
-      cityString =
-          '${firstCity.sentenceCase()} and ${secondCity.sentenceCase()}';
+  Text _buildTextWidget({@required String textOne, String textTwo = ''}) {
+    var textValue;
+    if (textTwo.isNotEmpty) {
+      textValue = '${textOne.sentenceCase()} and ${textTwo.sentenceCase()}';
     } else {
-      cityString = '${firstCity.sentenceCase()}';
+      textValue = '${textOne.sentenceCase()}';
     }
-
     return Text(
-      cityString,
+      textValue,
       style: kListTileTextStyle,
     );
+  }
+
+  Widget _buildCityOrPhoneWidget({String textOne, String textTwo}) {
+    if (textOne.isNotEmpty && textTwo.isNotEmpty) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildTextWidget(textOne: textOne),
+          _buildTextWidget(textOne: textTwo),
+        ],
+      );
+    } else {
+      return textOne.isNotEmpty
+          ? _buildTextWidget(textOne: textOne)
+          : _buildTextWidget(textOne: textTwo);
+    }
   }
 }
